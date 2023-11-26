b4c26c99c9edc67cd9d8dc47ab49faf29a57dba0
WalterGuerra <wsteven00@gmail.com>
2023-11-26 17:15:31 -0500

    edit profile model to have interests

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  interests: [{type: Schema.Types.ObjectId, ref: 'League'}]
 },{
   timestamps: true,
 })
