9c233d4df127b6ffb5e51302e668f884d2ec52ca
WalterGuerra <wsteven00@gmail.com>
2023-11-28 14:10:05 -0500

    edit notification model to have timestamp and expiration after a month

--- models/notification.js
+++ models/notification.js
@@ -15,7 +15,14 @@ const notificationSchema = new Schema({
   targetUser: { type: Schema.Types.ObjectId, ref: 'Profile'},
   blog: { type: Schema.Types.ObjectId, ref: 'Blog'},
   post: { type: Schema.Types.ObjectId, ref: 'Community.posts'},
-})
+},
+  { 
+    timestamps: true,
+    expireAfterSeconds: 30 * 24 * 60 * 60
+  }
+)
+
+notificationSchema.index({ createdAt: 1 }, { expireAfterSeconds: 0 })
 
 const Notification = mongoose.model('Notification', notificationSchema)
 
