b410c1df2da60a6e61816e20668dbb3917a735b1
WalterGuerra <wsteven00@gmail.com>
2023-11-28 13:38:57 -0500

    add update function to notifications controller and router

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -14,6 +14,20 @@ async function index(req, res) {
   }
 }
 
+async function update(req, res) {
+  try {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.notificationId,
+      { read: true },
+      { new: true }
+    )
+    res.status(200).json({ message: 'Notification marked as read', notification })
+  } catch (error) {
+    res.status(500).json(error)
+  } 
+}
+
 export {
   index,
+  update
 }
