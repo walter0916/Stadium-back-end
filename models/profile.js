4f069a883636dced079cde39ef24153eb79b1547
WalterGuerra <wsteven00@gmail.com>
2024-03-20 11:52:02 -0400

    edit profile model to have favorite teams and favorite players fields along with embedded schemas

--- models/profile.js
+++ models/profile.js
@@ -2,11 +2,48 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const teamSchema = new Schema({
+  teamId: Number,
+  name: String,
+  logo: String,
+  country: String,
+  founded: Number,
+  venueName: String,
+  venueCity: String,
+  venueCapacity: Number,
+  venueImage: String,
+})
+
+const playerSchema = new Schema({
+  playerId: Number,
+  name: String,
+  nationality: String,
+  age: Number,
+  height: String,
+  weight: String,
+  injured: Boolean,
+  photo: String,
+  goals: Number,
+  assists: Number,
+  teamName: String,
+  teamLogo: String,
+  appearances: Number,
+  position: String,
+  keyPasses: Number,
+  dribblesAttempted: Number,
+  dribblesSucceeded: Number,
+  tackles: Number,
+  blocks: Number,
+  interceptions: Number,
+})
+
 const profileSchema = new Schema({
   name: String,
   photo: String,
   interests: [{type: Schema.Types.ObjectId, ref: 'League'}],
-  joinedCommunities: [{type: Schema.Types.ObjectId, ref: 'Community'}]
+  joinedCommunities: [{type: Schema.Types.ObjectId, ref: 'Community'}],
+  favoriteTeams: [teamSchema],
+  favoritePlayers: [playerSchema]
 },{
   timestamps: true,
 })
